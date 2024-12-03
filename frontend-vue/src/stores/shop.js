36727bbbb42cc64dbe35f5e893048489cbf3536b
Shekul-Islam <shekul.bd201@gmail.com>
2024-12-03 15:50:52 +0600

    updated

--- frontend-vue/src/stores/shop.js
+++ frontend-vue/src/stores/shop.js
@@ -1,27 +1,74 @@
+// All Import File  Code Is Here......................................................................................................
 import { defineStore } from "pinia";
-import axiosInstance from "@/services/axiosService";
+import axiosInstance from "@/services/axiosService.js";
 
-export const shopStore = defineStore ("shop-page", {
-  state: ()=> ({
-    shops: {},
+export const useShop = defineStore("shop", {
+  // All Variable  Code Is Here.....................................................................................................
+  state: () => ({
+    products: [],
+    sideBar: [],
+    loading: false
   }),
 
-  persist: true,
-
   actions: {
-    async shopData () {
-      try {
-        const res = await axiosInstance.get('/products/shop-sidebar');
-        if(res?.data?.success) {
-          this.shops = res?.data?.result
-          console.log(this?.shops);
-        }
+    // API Calling Code Is Here.....................................................................................................
 
+    async getData(type = "", brand, category, subCategory, attributeIds, price, search, paginateSize) {
+        this.loading = true
+        
+      try {
+        const res = await axiosInstance.get(`/products`, {
+          params: {
+            type,
+            price,
+            paginate_size      : paginateSize,
+            brand_ids          : brand,
+            category_ids       : category,
+            sub_category_id    : subCategory,
+            attribute_value_ids: attributeIds,
+            search_key         : search,
+          },
+        });
+        if (res.status === 200) {
+          this.products = res.data.result;
+          return res.data.result
+        }
+      } catch (error) {
+        if (error.response.data) {
+          return new Promise((reject) => {
+            reject(error.response.data);
+          });
+        }
+      } finally {
+        this.loading = false
+      }
+    },
+
+    async sideBarData() {
+      try {
+        const res = await axiosInstance.get("/products/shop-sidebar");
+        if (res.status === 200) {
+          this.sideBar = res.data;
+        }
       } catch (error) {
         console.log(error);
         
       }
-    }
-  }
-})
+    },
 
+
+    async sideBarAttributeData() {
+      try {
+        const res = await axiosInstance.get("/attributes");
+        
+        if (res.status === 200) {
+           return res.data.result.data;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+  },
+});
+// All Function  Code Is Here.....................................................................................................
--- frontend-vue/src/views/pages/shop/Shop.vue
+++ frontend-vue/src/views/pages/shop/Shop.vue
@@ -1,28 +1,227 @@
 <script setup>
-import { onMounted } from 'vue';
-import { shopStore } from '@/stores';
-import { productStore } from '@/stores';
+import { useRoute, useRouter } from "vue-router";
+import { ref, onMounted, computed, watch  } from "vue";
+import { storeToRefs } from "pinia";
+import { useShop, useCart, useAuth, useCommonIsToggleFunctionality } from "@/stores";
 
-const shop = shopStore();
-const product = productStore();
+// All Variable  Code Is Here.....................................................................................................
+const route = useRoute();
+const router= useRouter();
+const auth = useAuth();
+const commonIsToggleFunctionality = useCommonIsToggleFunctionality();
+const cart = useCart();
+const { loadings } = storeToRefs(cart);
+const isloadings = ref(loadings);
+const quantityInput = ref(1);
+const getAttributeDatas = ref();
 
+const shop = useShop();
+const { products, sideBar, loading } = storeToRefs(shop);
+
+// const searchBrandQuery = ref("");
+// const searchCategoryQuery = ref("");
+
+// const productType = ref("");
+// const selectedBrandIds = ref([]);
+// const selectedCategoryIds = ref([]);
+// const selectedAttributeIds = ref([]);
+// const selectedSubCategoryIds = ref("");
+// const sortingPrice = ref([]);
+// const searchQuery = ref("");
+// const paginateSize = ref("");
+
+// const isloading = ref(loading);
+// const color = "white";
+// const size = "8px";
 
 
 const bannerImage = new URL("@/assets/images/single-banner.jpg", import.meta.url).href;
 
+// tab section
+const currentTab = ref("three");
 
-onMounted(()=> {
-  shop?.shopData();
-})
+const price = ref();
 
-onMounted(()=> {
-  product?.productsData();
-})
+// banner part start
+const banners = ref(''); 
+// banner part end
+
+// API Calling Code Is Here.....................................................................................................
+
+// function onEnter(el, done) {
+//   gsap.to(el, {
+//     opacity: 1,
+//     height: "1.6em",
+//     delay: el.dataset.index * 0.15,
+//     onComplete: done,
+//   });
+// }
+
+// watch(
+//   () => [...searchQuery.value],
+//   async (newValue, oldValue) => {
+//     if (newValue.length >= 3 || oldValue.length >= 3) {
+//       getProducts();
+//     }
+//   }
+// );
+
+// watch(()=> {
+//   console.log(products);
+// })
+
+// // All Function  Code Is Here.....................................................................................................
+
+// function addToCart(tProduct) {
+//   if (tProduct.offer_price != 0) {
+//     price.value = tProduct.offer_price;
+//   } else {
+//     price.value = tProduct.mrp;
+//   }
+
+//   if(sizeMrp.value || sizeOfferPrice.value){
+
+//     cart.addToCart({
+//       product_id: tProduct.id,
+//       name: tProduct.name,
+//       mrp: sizeMrp.value,
+//       offer_price: sizeOfferPrice.value,
+//       image: tProduct.image,
+//       size_id: sizeId.value,
+//       quantity: 1,
+//       free_shipping: tProduct.free_shipping,
+//     });
+    
+//   }else{
+    
+//     cart.addToCart({
+//       product_id: tProduct.id,
+//       name: tProduct.name,
+//       mrp: tProduct.mrp,
+//       // offer_price: tProduct.offer_price,
+//       image: tProduct.image,
+//       size_id: '',
+//       quantity: quantityInput.value,
+//       free_shipping: tProduct.free_shipping,
+//     });
+
+//   }
+
+
+//   notify.Success(`${tProduct.name} Successfully Added Your Cart product`);
+// }
+
+// const searchCategories = computed(() => {
+//   return shop.sideBar.result.categories.filter((category) => {
+//     return category.name
+//       .toLowerCase()
+//       .match(searchCategoryQuery.value.toLocaleLowerCase());
+//   });
+// });
+
+// const searchBrands = computed(() => {
+//   return shop.sideBar.result.brands.filter((brand) => {
+//     return brand.name.toLowerCase().match(searchBrandQuery.value.toLowerCase());
+//   });
+// });
+
+
+// const getProducts = () => {
+//   products.value = [];
+//   shop.getData(
+//     productType.value,
+//     selectedBrandIds.value,
+//     selectedCategoryIds.value,
+//     selectedSubCategoryIds.value,
+//     selectedAttributeIds.value,
+//     sortingPrice.value,
+//     searchQuery.value,
+//     paginateSize.value,
+//   );
+// };
+
+// function clearFilter(data) {
+//   if (data == "brand") {
+//     selectedBrandIds.value = [];
+//   }else if(data == "category") {
+//     selectedCategoryIds.value = [];
+//   } else {
+//     selectedAttributeIds.value = [];
+//   }
+
+//   getProducts();
+// }
+
+// Category wise product showing
+
+// const queryProducts = () => {
+//   selectedCategoryIds.value = [];
+//   selectedBrandIds.value = [];
+//   selectedAttributeIds.value = [];
+//   if (route.query.attribute) {
+//     selectedAttributeIds.value.push(route.query.attribute);
+//   }
+//   if (route.query.category) {
+//     selectedCategoryIds.value.push(route.query.category);
+//   }
+//   if (route.query.subCategory) {
+//     selectedSubCategoryIds.value = route.query.subCategory;
+//   }
+//   if (route.query.brand) {
+//     selectedBrandIds.value.push(route.query.brand);
+//   }
+//   if (route.query.recent) {
+//     productType.value = route.query.recent;
+//   }
+//   if (route.query.top) {
+//     productType.value = route.query.top;
+//   }
+//   if (route.query.feature) {
+//     productType.value = route.query.feature;
+//   }
+// };
+
+
+watch(
+  () => route.query.category || route.query.brand || route.query.subCategory,
+  async (newValue, oldValue) => {
+    console.log(newValue, oldValue);
+    queryProducts();
+    getProducts();
+  }
+);
+
+function closeCategorySideBar() {
+        $('.category-sidebar').removeClass('active');
+        $('.backdrop').fadeOut();
+}
+
+// attribute data 
+
+const getAttributeData = async() => {
+  getAttributeDatas.value = await shop.sideBarAttributeData();
+}
+
+onMounted(() => {
+  closeCategorySideBar()
+  getAttributeData()
+  // queryProducts();
+  shop.getData();
+  shop.sideBarData();
+  // getProducts();
+  $(document).ready(function () {
+    $(".venobox").venobox();
+  });
+});
+
 
 </script>
 
 <template>
+  
   <div>
+    <!-- {{ products?.data }}<br> -->
+    {{ shop }}
     <section class="inner-section single-banner" :style="{ background: 'url(' + bannerImage + ') center center no-repeat', }">
       <div class="container">
         <h2>Shop Page</h2>
@@ -37,12 +236,12 @@ onMounted(()=> {
             <div class="shop-widget">
               <h6 class="shop-widget-title">Filter by Price</h6>
               <form>
-                <div class="shop-widget-group" v-if="shop?.shops">
-                  <input type="text" :placeholder="`Min - ${shop?.shops?.min_sell_price}`">
-                  <input type="text" :placeholder="`Max - ${shop?.shops?.max_sell_price }`"/>
+                <div class="shop-widget-group" v-if="shop?.sideBar">
+                  <input type="text" :placeholder="`Min - ${shop?.sideBar?.min_sell_price}`">
+                  <input type="text" :placeholder="`Max - ${shop?.sideBar?.max_sell_price }`"/>
                 </div>
 
-                <button class="shop-widget-btn">
+                <button class="shop-widget-btn" @click.prevent="getProducts">
                   <i class="fas fa-search"></i><span>search</span>
                 </button>
               </form>
@@ -53,11 +252,13 @@ onMounted(()=> {
               <form>
                 <input class="shop-widget-search" type="text" placeholder="Search..."/>
                 <ul class="shop-widget-list shop-widget-scroll">
-                  <li v-for=" (brands, index) in shop?.shops?.brands" :key="index">
+                
+                  <li v-for=" (getAttributeData, index) in shop?.sideBar?.brands" :key="index">
+                    
                     <div class="shop-widget-content">
-                      <input type="checkbox" id="brand1"/><label for="brand1">{{brands?.name}}</label>
+                      <input type="checkbox" id="brand1"/><label for="brand1">{{getAttributeData?.name}}</label>
                     </div>
-                    <span class="shop-widget-number">({{brands?.products_count}})</span>
+                    <span class="shop-widget-number">({{getAttributeData?.products_count}})</span>
                   </li>
                 </ul>
 
@@ -115,10 +316,9 @@ onMounted(()=> {
                 </div>
               </div>
             </div>
-            
 
             <div class="row row-cols-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4" >
-              <div class="col"  v-for="(item, id) in product?.products?.data" :key="id">
+              <div class="col"  v-for="(product, id) in products.data" :key="id">
                 <div class="product-card">
                   <div class="product-media">
 
@@ -133,23 +333,23 @@ onMounted(()=> {
                     </button>
 
                     <a class="product-image" href="product-video.html">
-                      <img :src="item?.image" :alt="item.image"/>
+                      <img :src="product.image" alt="products.image"/>
                     </a>
                   </div>
 
                   <div class="product-content">
                     <h6 class="product-name">
-                      <a href="product-video.html">{{item?.name}}</a>
+                      <a href="product-video.html">{{product?.name}}</a>
                     </h6>
                     <h6 class="product-price">
 
-                      <span v-if="item?.variations?.data?.length">
-                        <span>{{ item?.variation_price_range?.min_price }} tk <span v-if="item?.variation_price_range?.max_sell_price != item?.variation_price_range?.min_sell_price"> - {{ item?.max_sell_price }} tk</span></span>
+                      <span v-if="product?.variations?.data?.length > 0">
+                        <span>{{ product?.variation_price_range?.min_price }} tk <span v-if="product?.variation_price_range?.max_sell_price != product?.variation_price_range?.min_sell_price"> - {{ product?.max_sell_price }} tk</span></span>
                       </span>
 
                       <span v-else>
-                        <span v-if="item?.offer_price !=0"><del class="text-danger">{{ item?.mrp }} tk</del></span>
-                        <span>{{ item?.offer_price != 0 ? offer_price : item?.mrp }} tk</span>
+                        <span v-if="product?.offer_price ==0"><del class="text-danger">{{ product?.mrp }} tk</del></span>
+                        <span>{{ product?.offer_price != 0 ? product?.offer_price : product?.mrp }} tk</span>
                       </span>
                       
                     </h6>
@@ -166,25 +366,25 @@ onMounted(()=> {
                 <div class="bottom-paginate">
                   <p class="page-info">Showing 12 of 60 Results</p>
                   <ul class="pagination">
-                    <li class="page-item">
+                    <li class="page-product">
                       <a class="page-link" href="#"
                         ><i class="fas fa-long-arrow-alt-left"></i
                       ></a>
                     </li>
-                    <li class="page-item">
+                    <li class="page-product">
                       <a class="page-link active" href="#">1</a>
                     </li>
-                    <li class="page-item">
+                    <li class="page-product">
                       <a class="page-link" href="#">2</a>
                     </li>
-                    <li class="page-item">
+                    <li class="page-product">
                       <a class="page-link" href="#">3</a>
                     </li>
-                    <li class="page-item">...</li>
-                    <li class="page-item">
+                    <li class="page-product">...</li>
+                    <li class="page-product">
                       <a class="page-link" href="#">60</a>
                     </li>
-                    <li class="page-item">
+                    <li class="page-product">
                       <a class="page-link" href="#"
                         ><i class="fas fa-long-arrow-alt-right"></i
                       ></a>
